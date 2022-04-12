5e9c6ee978e013cde5d0853dc9dd3b3d1253daf6
aemem <anamoliner.marques@gmail.com>
2022-04-12 11:48:37 +0000

    casi

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,31 +1,51 @@
 import React, { useState, useEffect } from "react";
 
 const Home = () => {
-	const [input, setInput] = useState("");
+	const [input, setInput] = useState({ label: "", done: false });
 	const [list, setList] = useState([]);
 	const [hoverOn, setHoverOn] = useState(false);
+	const [firstRender, setFirstRender] = useState(false);
 
-	useEffect(()=>{
+	useEffect(() => {
+		getList();
+	}, []);
 
-	})
+	useEffect(() => {
+		if (firstRender) {
+			updateList();
+		}
+	}, [list]);
 
-	const getTasks = async () => {
-		const response = await fetch("http://assets.breatheco.de/apis/fake/todos/user/aemem")
+	const getList = async () => {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/aemem"
+		);
 		const data = await response.json();
-		
-	}
+		setList(data);
+		setFirstRender(true);
+	};
 
-	const addTasks = async () => {
-		const response = await fetch("http://assets.breatheco.de/apis/fake/todos/user/aemem", {
-			method: 'PUT',
+	const updateList = async () => {
+		await fetch("https://assets.breatheco.de/apis/fake/todos/user/aemem", {
+			method: "PUT",
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(data)
-		})
-		const data = await response.json();
-		
-	}
+			body: JSON.stringify(list),
+		});
+	};
+
+	const ValidateInput = (e) => {
+		const labels = list.map((e) => e.label);
+		if (
+			e.key == "Enter" &&
+			e.target.value.trim() != "" &&
+			!labels.includes(e.target.value)
+		) {
+			setList([...list, input]);
+			e.target.value = "";
+		}
+	};
 
 	return (
 		<div className="container ">
@@ -35,32 +55,20 @@ const Home = () => {
 				</label>
 				<div className="d-flex">
 					<input
-						type="text"
-						className="form-control"
-						id="inputTask"
-						aria-describedby="emailHelp"
-						placeholder="enter task"
-						value={input}
 						onChange={(e) => {
-							setInput(e.target.value);
+							setInput({ ...input, label: e.target.value });
+						}}
+						onKeyDown={(e) => {
+							ValidateInput(e);
 						}}
 					/>
-					<button
-						type="button"
-						className="btn btn-light"
-						onClick={() => {
-							setList([...list, input]);
-							setInput("");
-						}}>
-						Submit
-					</button>
 				</div>
 			</div>
 
 			{list.map((e, i) => {
 				return (
 					<div key={i} className="row task mx-auto w-25 border">
-						<div className="col-10">{e}</div>
+						<div className="col-10">{e.label}</div>
 						<div
 							className="col-2"
 							onMouseEnter={() => setHoverOn(i)}
